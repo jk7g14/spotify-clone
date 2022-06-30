9e0bf178625b148861a22220597b19204725088c
Jaehun Kim <llyppp@revhat.com>
2022-06-30 21:13:02 +0900

    add gradient

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import GradientLayout from '../components/gradientLayout'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
-  return <div>home</div>
+  return (
+    <GradientLayout
+      color="green"
+      roundImage=""
+      subtitle="profile"
+      title="Scott Moss"
+      description="15 public playlists"
+    >
+      <div>home page</div>
+    </GradientLayout>
+  )
 }
 
 export default Home
